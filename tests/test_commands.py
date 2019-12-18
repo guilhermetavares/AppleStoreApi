4adccc567def90c626b258433a318da15dd81da6
Guilherme Tavares <zetavares.rib@gmail.com>
2019-12-18 16:54:49 -0300

    add csv export

---
+++ tests/test_commands.py
@@ -0,0 +1,13 @@
+import os
+from app.models import Application as MongoApplication
+
+
+def test_starts():
+    url = 'https://raw.githubusercontent.com/guilhermetavares/AppleStoreApi/master/tests/BaseTests.csv'
+    os.system(f"cd app/ && python start.py {url}")
+    assert MongoApplication.objects().first().apple_id == '281656475'
+
+
+def test_exports():
+    os.system(f"cd app/ && python export.py")
+    assert 1 == 1
